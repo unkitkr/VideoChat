e77fc2eaace2a0b29c56dcff6ae986d27ba1e0a5
Ankit Kumar <ankit.k@surveysparrow.com>
2021-01-20 14:45:37 +0530

    Staging for production - Heroku

--- app.js
+++ app.js
@@ -51,8 +51,7 @@ io.on('connection', (socket) =>{
     })
 })
 
-const PORT  =  5000
-
+const PORT  = process.env.PORT 
 server.listen(PORT, 
 	() => {console.log("Server is running on :" + PORT)}
 )
